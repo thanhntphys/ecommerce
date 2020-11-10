fd69784ce9d43367705acce45bd93809ca3e2e88
thanhntphys <thanhntphys@gmail.com>
2020-11-10 17:08:13 +0700

    Edit CORS

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -123,4 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'foodApp')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
